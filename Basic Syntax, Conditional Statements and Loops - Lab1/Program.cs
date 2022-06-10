650e822799671ee39256a8e499a3cf6d0f497511
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-10 19:46:43 +0300

    Add project files.

---
+++ Basic Syntax, Conditional Statements and Loops - Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic_Syntax__Conditional_Statements_and_Loops___Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startDate = DateTime.ParseExact(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
